635be78eea35d24e84f257d3a4b330424dc4d1e8
Tao Xu <nytaoxu@gmail.com>
2019-02-16 20:09:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -23,7 +23,7 @@ def apply_coupons(cart, coupons)
   # code here
   coupons.each do |coupon|
     cart.each do |name, info|
-      if name == coupon[:item] && info[:count] >= 
+      if name == coupon[:item] && info[:count] >= coupon[]
     end
   end
 end
