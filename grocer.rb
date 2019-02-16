a4f98526623954de06264135e076dac8f32cbcf5
Tao Xu <nytaoxu@gmail.com>
2019-02-16 20:06:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,29 @@
 def consolidate_cart(cart)
   # code here
+  result = Hash.new
+  cart.each do |item|
+    item.each do |name, info|
+      match = false
+      result.each do |r_name, status|
+        if r_name == name
+          match = true
+          status[:count] += 1
+        end
+      end
+      if match == false
+        info[:count] = 1
+        result[name] = info
+      end
+    end
+  end
+  result
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  coupons.each do |coupon|
+    cart.each do |
+  end
 end
 
 def apply_clearance(cart)
